fe6df2f5f50d944accba4629eefb21dc602fd86f
Titus Ramczykowski <ramczykowski@adrodev.de>
2012-04-23 15:25:42 +0200

    camera working

--- src/js/camera.js
+++ src/js/camera.js
@@ -1,19 +1,36 @@
 $('.upload-button').hide();
 
 $('.add-picture-button').bind('click', function(e) {
-  
   forge.file.getImage({
       width: 320,
       height: $('.page').height()
-    }, function(file) {
+    }, 
+    function(file) {
       // success
+      forge.logging.debug(file);
       var url = forge.file.URL(file, function(imageUrl) {
-      $('.picture-container').append('<img src="'+url+'">');
-      $('.add-picture-button').hide();
-      $('.upload-button').show();
+        if(imageUrl !== undefined) {
+          $('.picture-container').append('<img src="'+imageUrl+'">');
+          $('.add-picture-button').hide();
+          $('.upload-button').show();
+        }
+      });
+      $('.upload-button').data('picture-file',file);
     }, function(e) {
       // error
-    });
+    }
+  );
 
+  // fix tabbar
+  myTabBar.init();
+});
+
+$('.upload-button').bind('click', function(e) {
+  forge.request.ajax({
+    url: "http://example.com/file_upload",
+    files: [this.data('upload-button')],
+    success: function() {
+      window.location.href = 'list.html';
+    }
   });
 });
